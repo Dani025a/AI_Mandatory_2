e39b5e4d5b903dcdbddeacbc070151e28fa4cc82
Danijel <70882611+Dani025a@users.noreply.github.com>
2024-05-23 12:59:46 +0200

    Add files via upload

---
+++ ga_snake.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python
+
+from snake import SnakeGame
+from ga_controller import GAController
+
+if __name__ == '__main__':
+
+    game = SnakeGame()
+
+    controller = GAController(game)
+
+    game.run()
